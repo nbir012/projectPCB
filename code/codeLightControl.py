b578ccb05b49481474c109c49e0f42947305b133
nbir012 <nbir012@aucklanduni.ac.nz>
2017-06-04 07:10:23 +0000

    wrote loop to control LED on/off time

--- code/codeLightControl.py
+++ code/codeLightControl.py
@@ -1,18 +1,24 @@
 #code for turning LEDs on and off at set times.
 
-#import GPIO library for use of RPi's GPIO pins
+#import required libraries
 import RPi.GPIO as GPIO
-#import time library to allow use of time to control off/on
+import datetime 
 import time
+
 #use BCM naming convention
 GPIO.setmode(GPIO.BCM)
 #stop warnings appearing on screen
 GPIO.setwarnings(False)
-#use pin 22 to control LEDs
-GPIO.setup(22,GPIO.OUT)
-print "LED on"
-#use pin 22 to provide 
-GPIO.output(18,GPIO.HIGH)
-time.sleep(1)
-print "LED off"
-GPIO.output(18,GPIO.LOW)
+#set pin number and output
+pin = 22
+GPIO.setup(pin,GPIO.OUT)
+
+#run a loop where light turns on at 1000h for 12hrs.
+while True:
+    time = datetime.datetime.now().strftime("%H:%M")
+    if time == "10:00":
+        GPIO.output(pin,True)
+        time.sleep(43200)
+        GPIO.output(pin,False)
+    time.sleep(0.030)
+
